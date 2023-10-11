e036f1e32f1939323442196a86e2d8c0d6803c3b
= <=>
2023-10-11 21:42:08 +0200

    dizajn

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ app.get("/upload", (req, res) => {
     res.sendFile(__dirname + "/public/upload.html")
 })
 
+app.get("/video", (req, res) => {
+    res.sendFile(__dirname + "/public/video.html")
+})
+
+app.get("/channel", (req, res) => {
+    res.sendFile(__dirname + "/public/channel.html")
+})
+
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 })
